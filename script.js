61a850393e8667d1e8696991009d413c3d954256
Driss.ABSIKE <Driss.ABSIKE@berger-levrault.com>
2021-12-08 11:16:43 +0100

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+// Declaration des inputs select, Button...Par leurs ID
 const select = document.querySelectorAll(".currency");
 const btn = document.getElementById("btn");
 const num = document.getElementById("num");
 const ans = document.getElementById("ans");
 
+//Utilisation api frankfurter => envoyé les données vers la variable data
 fetch("https://api.frankfurter.app/currencies")
   .then((data) => data.json())
   .then((data) => {
     display(data);
   });
 
+  //Function qui affiche les données sur Select Tag
 function display(data) {
   const entries = Object.entries(data);
   for (var i = 0; i < entries.length; i++) {
@@ -17,6 +20,7 @@ function display(data) {
   }
 }
 
+//Traitement de button pour la conversion des devises
 btn.addEventListener("click", () => {
   let currency1 = select[0].value;
   let currency2 = select[1].value;
@@ -29,6 +33,7 @@ btn.addEventListener("click", () => {
   }
 });
 
+//La fonction qui utilise l'api pour la conversion de USD vers EURO par exemple
 function convert(currency1, currency2, value) {
   const host = "api.frankfurter.app";
   fetch(
@@ -36,7 +41,7 @@ function convert(currency1, currency2, value) {
   )
     .then((val) => val.json())
     .then((val) => {
-      console.log(Object.values(val.rates)[0]);
+      //console.log(Object.values(val.rates)[0]);
       ans.value = Object.values(val.rates)[0];
     });
 }
